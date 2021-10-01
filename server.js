91feee6811a1c00e42b5e86f37e9a7a90fe7a5c1
ZAYD ELIMRANI <plutorider47@gmail.com>
2021-10-01 12:37:39 +0100

    changing for heroku

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ connection.once('open', () => {
 const exercisesRouter = require('./routes/exercises');
 const usersRouter = require('./routes/users');
 
-app.use('/', exercisesRouter);
+app.use('/exercises', exercisesRouter);
 app.use('/users', usersRouter);
 
 app.listen(port, () => {
