0457a8df2c5381a4b2461ee6e73a834ce71e033e
ZAYD ELIMRANI <plutorider47@gmail.com>
2021-10-01 12:53:23 +0100

    Testing cors policy with heroku

--- server.js
+++ server.js
@@ -7,7 +7,12 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+var corsOptions = {
+  origin: 'http://localhost:5500',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
